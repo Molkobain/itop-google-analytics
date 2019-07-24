b7250190d3dc40f6c9ddea2b14df6dbddd21d2ac
Molkobain <lajarige.guillaume@free.fr>
2019-07-24 12:16:22 +0200

    :arrow_up: Update dependencies to include their fixes

--- src/molkobain-google-analytics/module.molkobain-google-analytics.php
+++ src/molkobain-google-analytics/module.molkobain-google-analytics.php
@@ -14,7 +14,7 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'molkobain-google-analytics/1.3.0',
+	'molkobain-google-analytics/1.3.1',
 	array(
 		// Identification
 		//
@@ -25,7 +25,7 @@ SetupWebPage::AddModule(
 		//
 		'dependencies' => array(
 			'molkobain-handy-framework/1.2.4',
-			'molkobain-newsroom-provider/1.0.0',
+			'molkobain-newsroom-provider/1.0.1',
 		),
 		'mandatory' => false,
 		'visible' => true,
